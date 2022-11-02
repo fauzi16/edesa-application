2df36a7791c0aacbc17dda3cb899779e2f9ed566
fauzi16 <fauziachmad.af@gmail.com>
2022-11-02 15:33:07 +0700

    get user by username

---
+++ source/backend/src/main/java/com/edesa/controller/standard/dtpl/master/FindByUserNameParam.java
@@ -0,0 +1,16 @@
+package com.edesa.controller.standard.dtpl.master;
+
+public class FindByUserNameParam {
+    
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    
+}
--- source/backend/src/main/java/com/edesa/controller/standard/dtpl/master/UserController.java
+++ source/backend/src/main/java/com/edesa/controller/standard/dtpl/master/UserController.java
@@ -2,7 +2,9 @@ package com.edesa.controller.standard.dtpl.master;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.edesa.controller.AbstractController;
@@ -18,5 +20,11 @@ public class UserController extends AbstractController<User, Long, UserService> 
     UserController(UserService service) {
         super(service, User.class, Long.class);
     }
+
+    @RequestMapping(value = "/find-by-username", method = RequestMethod.POST)
+    public User findUserByUsername(@RequestBody FindByUserNameParam username) {
+        return this.service.getDao().findByUsername(username.getUsername());
+    }
+
     
 }
